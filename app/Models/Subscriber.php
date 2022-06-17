5b3c0ffb31f3bc637a19b389af50b1f669fd2df7
Adrian Leong <leongadrian36@gmail.com>
2022-06-17 12:17:18 +0800

    subscriber model

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Subscriber extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    public $fillable = [
+        'email'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }

bd7609c4b713bc265367e74b10773de6783170cd
Adrian Leong <leongadrian36@gmail.com>
2022-06-17 16:49:28 +0800

    wire model $email

--- app/Http/Livewire/LandingPage.php
+++ app/Http/Livewire/LandingPage.php
@@ -3,9 +3,18 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Log;
 
 class LandingPage extends Component
 {
+
+    public $email;
+
+    public function subscribe()
+    {
+        Log::debug($this->email);
+    }
+
     public function render()
     {
         return view('livewire.landing-page');
--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -31,9 +31,9 @@
         <div class="m-auto bg-pink-500 shadow-2xl rounded-xl p-8">
             <p class="text-white text-5xl font-extrabold text-center">Let's do it!</p>
 
-            <form action="" class="flex flex-col items-center p-24">
+            <form action="" class="flex flex-col items-center p-24" wire:submit.prevent='subscribe'>
                 <x-input class="px-5 py-3 w-80 border border-blue-400" type="email" name="email"
-                    placeholder="Email Address"></x-input>
+                    placeholder="Email Address" wire:model="email"></x-input>
                 <span class="text-gray-100 text-xs">We will send you a confirmation email.</span>
                 <x-button class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center">Get In
                 </x-button>

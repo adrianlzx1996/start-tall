94c68ba90eed8a66d70222b5095e0254bf1aaf1e
Adrian Leong <leongadrian36@gmail.com>
2022-06-17 12:17:25 +0800

    subscriber factory

--- database/factories/SubscriberFactory.php
+++ database/factories/SubscriberFactory.php
@@ -17,7 +17,22 @@ class SubscriberFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
         ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return static
+     */
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
     }
 }

1736abc4e232cc9ccfd3037a110f1b4bbafe4447
Adrian Leong <leongadrian36@gmail.com>
2022-06-20 16:32:36 +0800

    save subscriber into db

--- app/Http/Livewire/LandingPage.php
+++ app/Http/Livewire/LandingPage.php
@@ -3,20 +3,30 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Subscriber;
 use Illuminate\Support\Facades\Log;
 
 class LandingPage extends Component
 {
 
-    public $email;
+    public $email = 'test@test.com';
 
     public function subscribe()
     {
-        Log::debug($this->email);
+        $subscriber = Subscriber::create([
+            'email' => $this->email,
+        ]);
+
+        $this->resetInput();
     }
 
     public function render()
     {
         return view('livewire.landing-page');
+    }
+
+    public function resetInput()
+    {
+        $this->reset('email');
     }
 }

9e71d41c578604fa31278babebf1f3bdf9977bf2
Adrian Leong <leongadrian36@gmail.com>
2022-06-20 17:35:47 +0800

    hide modal on loading

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,6 +1,6 @@
 @props(['trigger'])
 
-<div class="fixed top-0 flex items-center w-full h-full bg-gray-900 bg-opacity-60" x-show="{{ $trigger }}"
+<div class="fixed top-0 flex items-center w-full h-full bg-gray-900 bg-opacity-60" x-show="{{ $trigger }}" x-cloak
     @click.self="{{ $trigger }} = false" @keydown.escape.window="{{ $trigger }} = false" x-transition>
     <div {{ $attributes->merge(['class' => 'p-8 m-auto shadow-2xl rounded-xl']) }}>
         {{ $slot }}
